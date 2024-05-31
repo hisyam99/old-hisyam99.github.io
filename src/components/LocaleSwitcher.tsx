b601fccac21d2186b1dfaecf7e430de5fe5d7de6
Muhammad Hisyam Kamil <hisyamkamil99@gmail.com>
2024-05-31 08:13:39 +0700

    Fix minor bugs.

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -1,18 +1,34 @@
+import { useEffect, useState } from "react";
 import LocaleSwitcherSelect from "./LocaleSwitcherSelect";
 import { useRouter } from "next/navigation";
 import { SelectItem } from "@/components/ui/select";
 
-
 export default function LocaleSwitcher() {
   const router = useRouter();
+  const [defaultLocale, setDefaultLocale] = useState<string | null>(null);
+
+  useEffect(() => {
+    const savedLocale = localStorage.getItem("locale");
+    if (savedLocale) {
+      setDefaultLocale(savedLocale);
+    } else {
+      setDefaultLocale("en");
+    }
+  }, []);
 
   function handleLocaleChange(value: string) {
+    localStorage.setItem("locale", value);
+    setDefaultLocale(value);
     router.push(`/${value}`);
+  }
+
+  if (defaultLocale === null) {
+    return null;
   }
 
   return (
     <LocaleSwitcherSelect
-      defaultValue={"en"}
+      defaultValue={defaultLocale}
       label={"Select Language"}
       onSelectChange={handleLocaleChange}
     >
